4c9c30e8ea5fd515fad346b8c09cf20d5b173eff
AkshayWay <38669737+AkshayWay@users.noreply.github.com>
2021-11-30 22:21:56 +0530

    Data for services page
    Data for services page

---
+++ src/Sdata.jsx
@@ -0,0 +1,19 @@
+import { getDefaultNormalizer } from "@testing-library/dom";
+import Clean from "../src/Images/clean_service.svg";
+import Wifi from "../src/Images/wifi_service.svg";
+
+const Sdata = [
+  {
+    imgSrc: Clean,
+    title: "Cleanliness",
+  },
+  {
+    imgSrc: Wifi,
+    title: "Internet Service",
+  },
+  {
+    imgSrc: Wifi,
+    title: "Internet Service",
+  },
+];
+export default Sdata;
