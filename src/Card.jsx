42eb95e145841b71f5f3b9eafaed49f97644f373
AkshayWay <38669737+AkshayWay@users.noreply.github.com>
2021-11-30 22:21:28 +0530

    Services page added
    Services page added

---
+++ src/Card.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import React from "react";
+
+import homeImage from "../src/Images/hostel_page.svg";
+import Common from "./Common";
+
+const Card = (props) => {
+  return (
+    <>
+      <div className="col-md-4 col-10 mx-auto">
+        <div className="card">
+          <img src={props.imgSrc} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
